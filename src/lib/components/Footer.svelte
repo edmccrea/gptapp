7e921f6faaaece0d6cea0926d46930c238e6451d
edmccrea <ed-mccrea@hotmail.co.uk>
2023-03-24 21:49:54 +0100

    feat: prepare model selector for gpt 4 access

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,34 @@
-<footer class="my-10 flex flex-col items-center">
+<script lang="ts">
+  // import { createEventDispatcher } from "svelte";
+
+  // const dispatch = createEventDispatcher<{ selectModel: string }>();
+
+  let selected: string;
+
+  const models = [
+    {
+      name: "GPT-3.5",
+      value: "gpt-3.5-turbo",
+    },
+    {
+      name: "GPT-4",
+      value: "gpt-4",
+    },
+  ];
+</script>
+
+<footer class="my-10 flex flex-col items-center relative w-full">
+  <select
+    bind:value={selected}
+    name=""
+    id=""
+    class="bg-transparent w-fit lg:absolute lg:right-5 mb-5 border border-gray-400 rounded-md pl-3 pr-10 py-2 hover:cursor-pointer focus:outline-none"
+  >
+    {#each models as model}
+      <option value={model.value}>{model.name}</option>
+    {/each}
+  </select>
+
   <p>
     Created by <a
       href="https://github.com/edmccrea"
@@ -23,3 +53,12 @@
     >
   </p>
 </footer>
+
+<style>
+  select {
+    appearance: none;
+    background-image: url("/chevron-down.svg");
+    background-repeat: no-repeat, repeat;
+    background-position: right 0.7em top 50%, 0 0;
+  }
+</style>
